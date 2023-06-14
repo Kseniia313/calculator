e0405bf93d5cb77a4a41f89dc8654550e03b5d24
Kseniia313 <kshome@mail.ru>
2023-06-14 11:43:15 +0300

    Доработки первая часть

--- CalculatorServiceImpl.java
+++ CalculatorServiceImpl.java
@@ -3,7 +3,7 @@ package pro.sky.calculator;
 import org.springframework.stereotype.Service;
 
 @Service
-public class CalculatorServiceImpl implements CalculatorServiceInterfeis{
+public class CalculatorServiceImpl implements CalculatorServiceInterfeis {
 
     public String helloCalculator() {
         return "Добро пожаловать в калькулятор";
@@ -12,25 +12,35 @@ public class CalculatorServiceImpl implements CalculatorServiceInterfeis{
     public String plus(Integer num1, Integer num2) {
         if ((num1 == null) || (num2 == null)) {
             return "Вам необходимо ввести число";
-        } else return num1 + " + " + num2 + " = " + (num1 + num2);
+        } else {
+            return num1 + " + " + num2 + " = " + (num1 + num2);
+        }
     }
 
-        public String minus (Integer num1, Integer num2){
-            if ((num1 == null) || (num2 == null)) {
-                return "Вам необходимо ввести число";}
-          else  return num1 + " - " + num2 + " = " + (num1 - num2);
-        }
-        public String divide (Integer num1, Integer num2){
-                if ((num1 == 0) || (num2 == 0)) {
-                    return "ArithmeticException";}
-                else return num1 + " / " + num2 + " = " + (num1 / num2);
-        }
-        public String multiply (Integer num1, Integer num2){
-            if ((num1 ==null) || (num2 == null)) {
-                return "Вам необходимо ввести число";}
-          else return num1 + " * " + num2 + " = " + (num1 * num2);
+    public String minus(Integer num1, Integer num2) {
+        if ((num1 == null) || (num2 == null)) {
+            return "Вам необходимо ввести число";
+        } else {
+            return num1 + " - " + num2 + " = " + (num1 - num2);
         }
     }
+
+    public String divide(Integer num1, Integer num2) {
+        if (num2 == 0) {
+            return "ArithmeticException";
+        } else {
+            return num1 + " / " + num2 + " = " + (num1 / num2);
+        }
+    }
+
+    public String multiply(Integer num1, Integer num2) {
+        if ((num1 == null) || (num2 == null)) {
+            return "Вам необходимо ввести число";
+        } else {
+            return num1 + " * " + num2 + " = " + (num1 * num2);
+        }
+    }
+}
 
 
 
